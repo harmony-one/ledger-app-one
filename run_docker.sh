dba20ba5a0f8ebb53d197babe755dcd7e8e6a9f3
coolcottontail <coolcottontail@gmail.com>
2019-09-05 08:03:25 -0700

    clean everything to make a PR

--- run_docker.sh
+++
@@ -1,1 +0,0 @@
-docker run  -it  --rm --privileged -v /run/udev:/run/udev -v /dev:/dev:ro   -v .:/one-ledger -w /one-ledger ledger-chain 
--- testvectors/generated_c_array.py
+++
@@ -1,6 +0,0 @@
-import sys
-ll=[]
-for i in range(0,40,2):
-    ll.append(sys.argv[1][i:i+2])
-print(",0x".join(ll))
-
